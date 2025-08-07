94bb769b378c00ca46a8afb084376bff07950b71
Guillermo Sánchez-Cid <guisanbue@gmail.com>
2025-08-07 14:04:39 +0200

    add working mock mutations

---
+++ workflow/scripts/format_pvi_intersect.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import itertools
+import sys
+import argparse
+
+def format_intersect(intersect_list, samplesheet, pvi_prep, phyclone):
+    """
+        Build DataFrames for Pyclone-VI and Phyclone, one per sample.
+
+    Args:
+        intersect_list (str): spaced list of intersect dataframes of each sample (TSV)
+        samplesheet (str): Path to the study samplesheet
+        pvi_prep (str): Path to formatted dataframe for Pyclone-VI (TSV).
+        phyclone (str):  Path to formatted dataframe for Phyclone (TSV).
+
+    Return:
+        Dictionary of samples' dataframes to plot the VAF Heatmap
+    """
+    # List of pvi prep samples
+
+    pvi_preps = [pd.read_csv(file, sep='\t') for file in intersect_list]
+
+    # Concatenate
+    combined_pvi = pd.concat(pvi_preps, ignore_index=True)
+
+    # Drop artifactual duplicates
+    combined_pvi_dedup = combined_pvi.drop_duplicates()
+
+    # Add mock mutations
+
+    ## Scan available mutations
+    mutations = set(combined_pvi_dedup['mutation_id'].unique())
+    samples = set(combined_pvi_dedup['sample_id'].unique())
+
+    ## Get all hypothetical combinations 
+    mutations_complete = set(itertools.product(mutations, samples))
+
+    ## Get existing mutations
+    true_mutations = set(zip(combined_pvi_dedup['mutation_id'], combined_pvi_dedup['sample_id']))
+
+    ## Get missing mutations
+    missing_mutations = mutations_complete - true_mutations
+
+    # Add mock mutations for missing ones
+    mock_entries = []
+    for mutation_id, sample_id in missing_mutations:
+        mock_entries.append({
+            'mutation_id': mutation_id,
+            'sample_id': sample_id,
+            'ref_counts': 0,
+            'alt_counts': 0,
+            'major_cn': 2,
+            'minor_cn': 2,
+            'normal_cn': 1 if str(mutation_id).lower().startswith('chry') else 2
+        })
+
+
+    ## Add mock mutations df to the original df
+    mock_df = pd.DataFrame(mock_entries)
+    complete_df = pd.concat([combined_pvi_dedup, mock_df], ignore_index=True)
+
+    ## Sort completed df by mutation_id and sample_id
+    complete_df.sort_values(by=['mutation_id', 'sample_id'], inplace=True)
+
+    # Add tumour_content aka purity
+    samplesheet = pd.read_csv(samplesheet)
+    tumour_content_dict = dict(zip(samplesheet['sample_id'], samplesheet['tumour_content']))
+    complete_df['tumour_content'] = complete_df['sample_id'].map(tumour_content_dict)
+
+    # Save pyclone-vi formatted intersect df
+    complete_df.to_csv(pvi_prep, sep='\t', index=False)
+
+    # Subset and save intersect df for phyclone input format
+    phyclone_df = complete_df[['mutation_id', 'sample_id', 'ref_counts', 'alt_counts', 'major_cn', 'minor_cn', 'normal_cn', 'tumour_content']]
+    phyclone_df.to_csv(phyclone, sep='\t', index=False)
+
+    return complete_df, phyclone_df
+
+
+if __name__ == '__main__':
+    input_parser = argparse.ArgumentParser()
+    input_parser.add_argument("--intersect_list", action='store', nargs='+', required=True)
+    input_parser.add_argument("--samplesheet", action='store', required=True)
+    input_parser.add_argument("--pvi_prep", action='store', required=True)
+    input_parser.add_argument("--phyclone", action='store', required=True)
+    args = input_parser.parse_args() 
+    
+    format_intersect(args.intersect_list, args.samplesheet, args.pvi_prep, args.phyclone)
