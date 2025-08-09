64e370ff8a84e3319b485a2e9de4c139fcced457
Guillermo Sánchez-Cid <guisanbue@gmail.com>
2025-08-09 13:42:47 +0200

    add filter to mock mutations

--- workflow/scripts/vep_formatting.py
+++ workflow/scripts/vep_formatting.py
@@ -7,34 +7,79 @@ from clonucopya_tools import chr_to_num
 
 
 
+def filt_mock_mutations(pvi_in, pvi_out):
 
-def process_pyclone_muts_clones(pvi_file, sample_id, out_dir):
+    """
+    Filter out mock mutations before formatting vep input.
+    
+    Args:
+        pvi_int: Path to input TSV file containing PyClone-VI input
+        pvi_out: Path to input TSV file containing PyClone-VI results
+    
+    Returns:
+        DataFrame contained filtered Pyclone-VI output witout mock mutations
+    """
+
+    # Filter mock mutations out of Pyclone-VI input
+    pvi_mock = pvi_in[((pvi_in.ref_counts == 0) & (pvi_in.alt_counts == 0))]
+
+
+    # Filter mock mutations out of Pyclone-VI output using filtered pvi input
+    pvi_filt = (
+        pvi_out
+        .merge(
+            pvi_mock[['mutation_id', 'sample_id']],
+            on=['mutation_id', 'sample_id'],
+            how='left',
+            indicator=True
+        )
+        .query('_merge == "left_only"')
+        .drop(columns=['_merge'])
+    )
+    return pvi_filt
+
+
+
+
+def process_pyclone_muts_clones(pvi_in, pvi_out, study, out_dir):
     """
     Format output from PyClone-VI to VEP standard as input for ensembl-vep.
     
     Args:
-        pvi_file: Path to input TSV file containing PyClone-VI results
-        sample_id: Name of the sample
+        pvi_int: Path to input TSV file containing PyClone-VI input
+        pvi_out: Path to input TSV file containing PyClone-VI results
+        study: Name of the sample
         out_dir: Path to output directory
     
     Returns:
         Dictionary mapping cluster ids to dataframes containing mutation information
     """
+
+    
     # Check and create output directory if it doesn't exist
     out_path = Path(out_dir)
     out_path.mkdir(parents=True, exist_ok=True)
+
+    # Load PyClone-VI input
+    try:
+        pvi_prep = pd.read_csv(pvi_in, sep='\t')
+    except Exception as e:
+        raise ValueError(f"Failed to read PyClone-VI input file: {e}")
     
     # Load PyClone-VI output
     try:
-        pvi_data = pd.read_csv(pvi_file, sep='\t')
+        pvi_results = pd.read_csv(pvi_out, sep='\t')
     except Exception as e:
-        raise ValueError(f"Failed to read PyClone-VI file: {e}")
+        raise ValueError(f"Failed to read PyClone-VI output file: {e}")
+
+    # Filter mock mutations out of Pyclone-VI output
+    pvi_filt = filt_mock_mutations(pvi_prep, pvi_results)
 
     # Initialize cluster dictionary
     clone_dataframes: Dict[int, List[Dict]] = {}
     
     # Process mutations
-    for mut in pvi_data['mutation_id'].unique():
+    for mut in pvi_filt['mutation_id'].unique():
         try:
             # Process chromosome prefix and split mutation components
             chrom, pos, ref, alt = mut.split(':')
@@ -48,7 +93,7 @@ def process_pyclone_muts_clones(pvi_file, sample_id, out_dir):
                 # Substitution
                 end = int(pos) + len(ref) - 1
 
-            clone_id = pvi_data.loc[pvi_data['mutation_id'] == mut, 'cluster_id'].iloc[0]
+            clone_id = pvi_filt.loc[pvi_filt['mutation_id'] == mut, 'cluster_id'].iloc[0]
 
             if clone_id not in clone_dataframes:
                 clone_dataframes[clone_id] = []
@@ -63,7 +108,8 @@ def process_pyclone_muts_clones(pvi_file, sample_id, out_dir):
               })
         except Exception as e:
             raise ValueError(f"Error processing mutation {mut}: {e}")
-    
+
+    # Sort clone dfs and save them in the same output directory
     result: Dict[int, pd.DataFrame] = {}
     for clone_id, variants in clone_dataframes.items():
         df = pd.DataFrame(variants)
@@ -71,7 +117,7 @@ def process_pyclone_muts_clones(pvi_file, sample_id, out_dir):
         df['chr_num'] = df['chr'].apply(chr_to_num)
         df.sort_values(by =['chr_num','start'], inplace=True)
         df.drop(columns=['chr_num'], inplace=True)
-        output_file = out_path / f"{sample_id}_cluster_{clone_id}.tsv"
+        output_file = out_path / f"{study}_cluster_{clone_id}.tsv"
         df.to_csv(output_file, sep='\t', index=False, header=False)
         result[clone_id] = df
     
@@ -79,12 +125,13 @@ def process_pyclone_muts_clones(pvi_file, sample_id, out_dir):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("--pvi_prep",  action='store', required=True)
     parser.add_argument("--pvi_data",  action='store', required=True)
-    parser.add_argument("--sample_id", action='store', required=True)
+    parser.add_argument("--study", action='store', required=True)
     parser.add_argument("--out_dir", action='store', required=True)
     
     args = parser.parse_args()
-    process_pyclone_muts_clones(args.pvi_data, args.sample_id, args.out_dir)
+    process_pyclone_muts_clones(args.pvi_prep, args.pvi_data, args.study, args.out_dir)
 
 
 
