e18cca516dea72f3e1079f67c0c2c0bfae9fc3a8
Guillermo Sánchez-Cid <guisanbue@gmail.com>
2025-08-22 14:04:44 +0200

    add Impact filtering and structured output

--- workflow/scripts/draw_vaf_heatmap.py
+++ workflow/scripts/draw_vaf_heatmap.py
@@ -9,8 +9,39 @@ from clonucopya_tools import chr_to_num
 
 
 
+def even_distribution_tolerant(df_heatmap, n_samples=100):
+    """
+    Sampling using even distribution that deals with al sizes of heatmaps.
+    If the Dataframe has less rows than n_sampes, it returns the whole dataframe.
 
-def build_heatmap_df(tree_df,pvi_out, mut_dir):
+    Args:
+    df_heatmap: DataFrame of the VAF heatmap
+    Return:
+    Sampled Dataframe
+    """
+    total_rows = len(df_heatmap)
+    
+    # If DataFrame equal or smaller than the threshold
+    if total_rows <= n_samples:
+        return df_heatmap.copy()
+    
+    # Sampling for larger dataframes
+    indices = np.linspace(0, total_rows-1, n_samples, dtype=int)
+    
+    # Drop duplicates preserving the order
+    unique_indices = []
+    seen = set()
+    for idx in indices:
+        if idx not in seen:
+            unique_indices.append(idx)
+            seen.add(idx)
+
+    return df_heatmap.iloc[unique_indices]
+
+
+
+
+def build_heatmap_df(tree_df, pvi_out, mut_dir, gene_alterations):
 
     """
     Build Daframe for VAF Heatmap, one per sample.
@@ -18,14 +49,18 @@ def build_heatmap_df(tree_df,pvi_out, mut_dir):
     Args:
         tree_df (str): Path to the phyclone TSV output
         pvi_out (str): Path to the pyclone-vi TSV output
-        mut_dir (str): Path to the files from mutation_prep project.
+        mut_dir (str): Path to the files from mutation_prep study.
+        gene_alterations (str): Path to the gene alterations file of the study
 
     Return:
-        Dictionary of samples' dataframes to plot the VAF Heatmap
+        Dictionary of dictioraries with complete and sampled data of samples' dataframes to plot the VAF Heatmap
         
     """
     # Load Phyclone output
-    phy_df = pd.read_table(tree_df)
+    try:
+        phy_df = pd.read_table(tree_df)
+    except Exception as e:
+        raise ValueError(f"Error reading Phyclone file: {e}")
     
     # Obtain cluter-clone equivalences
     phy_clones = phy_df[['clone_id', 'cluster_id']].drop_duplicates()
@@ -33,7 +68,10 @@ def build_heatmap_df(tree_df,pvi_out, mut_dir):
     
     # PYCLONE-VI INFO
     # Load Pyclone-VI output
-    pvi_df = pd.read_table(pvi_out)
+    try:
+        pvi_df = pd.read_table(pvi_out)
+    except Exception as e:
+        raise ValueError(f"Error reading Pyclone-VI file: {e}")
     
     # Subset Pyclone-VI dataframe by samples
     pvi_dict = {sample_id: sub_df for sample_id, sub_df in pvi_df.groupby('sample_id')}
@@ -50,18 +88,30 @@ def build_heatmap_df(tree_df,pvi_out, mut_dir):
     
     # Store all mutations dataframe in a dict
     mut_dict = {}
-    
+
     for file in mut_files:
         file_name = os.path.basename(file)
         sampleid = file_name.split('_prep.mut.tsv')[0]
-        sample_df = pd.read_table(file)
+        
+        try:
+            sample_df = pd.read_table(file)
+        except Exception as e:
+            raise ValueError(f"Error reading {file_name}: {e}")
         
         mut_dict[sampleid] = sample_df
     
+    # GENE ALTERATIONS INFO
+    # Load gene alterations table from report components
+    try:
+        gene_alt = pd.read_table(gene_alterations)
+    except Exception as e:
+        raise ValueError(f"Error reading {gene_alterations}: {e}")
+        
     
     # INNER JOINT OF MUT AND PVI
     # Obtain a dict of samples' dataframe with ['mutation_id','cluster_id','VAF']
     heatmap_dict = {}
+    heatmap_dict_sampled = {}
     
     for sample_id in mut_dict:
         merged = mut_dict[sample_id][['mutation_id', 'VAF']].merge(
@@ -69,7 +119,6 @@ def build_heatmap_df(tree_df,pvi_out, mut_dir):
             on='mutation_id',
             how='inner')
         heatmap_dict[sample_id] = merged[['mutation_id', 'cluster_id', 'VAF']]
-    
     
     
     # FORMAT DATAFRAMES AND INPUT VALUES
@@ -84,81 +133,119 @@ def build_heatmap_df(tree_df,pvi_out, mut_dir):
         
         # Add mutations that are absent in each sample and reindex
         df_reindexed = pivoted.reindex(all_muts_list, fill_value=0)
+        
     	# Sort mutations by clone, chr, and pos
+        df_reindexed = df_reindexed.sort_index()
         temp_df = pd.DataFrame(index=df_reindexed.index)
         temp_df['chr_num'] = temp_df.index.to_series().apply(lambda x: chr_to_num(x.split(':')[0]))
         temp_df['pos'] = temp_df.index.to_series().apply(lambda x: int(x.split(':')[1]))
     
-        # Ordenar por cromosoma y posición
+        # Sort temporary Dataframe mutations by chromosome and position
         temp_df_sorted = temp_df.sort_values(by=['chr_num', 'pos'])
-    
-        # Reordenar el DataFrame principal usando el índice ordenado
+        
+        # Sort the main Dataframe using sorted index from temporary DataFrame
         df_reindexed = df_reindexed.loc[temp_df_sorted.index]
+
+        ## Remove with 0 VAF in all clones
+
+        df_no_empty_muts = df_reindexed.loc[(df_reindexed != 0).any(axis=1)]
+
+        # Reset index to convert mutation_id into a column
+        temp_mut_idx_df = df_no_empty_muts.reset_index()
+
+        # Merge sorted index DataFrame with gene_alteration Dataframe to obtain Gene Symbol information
+        mut_gene_idx_df = temp_mut_idx_df.merge(gene_alt[['Mutation ID', 'Gene Symbol', 'Impact']], 
+            left_on='mutation_id', 
+            right_on='Mutation ID', 
+            how='left')
+
+        # Only keep mutations with High or Moderate Impact annotated by vep
+        mut_gene_idx_df = mut_gene_idx_df[(mut_gene_idx_df['Impact'] == 'MODERATE') | (mut_gene_idx_df['Impact'] == 'HIGH')]
     
+        # Create a new mutation_id index combining mutation_id and its gene symbol
+        mut_gene_idx_df['mut_gene_idx'] = mut_gene_idx_df.apply(
+            lambda row: f"{row['mutation_id']} - {row['Gene Symbol']}" 
+            if pd.notna(row['Gene Symbol']) 
+            else row['mutation_id'], 
+            axis=1)
+    
+        # Set mut_gene_idx as the new index and drop auxiliary columns
+        mut_gene_idx_df = mut_gene_idx_df.set_index('mut_gene_idx').drop(['mutation_id', 'Mutation ID', 'Gene Symbol', 'Impact'], axis=1)
+
+        
         # Transform cluster to clones
-        df_renamed = df_reindexed.rename(columns=cluster_to_clone)
+        df_renamed = mut_gene_idx_df.rename(columns=cluster_to_clone)
     
         # Reorder clones. Sometimes, clusters and clones dont have the same order
         ordered_cols = sorted(df_renamed.columns)
     
-        # Update original dataframe
+        # Update original dataframes
         heatmap_dict[sample_id] = df_renamed[ordered_cols]
+        heatmap_dict_sampled[sample_id] = even_distribution_tolerant(df_renamed[ordered_cols])
 
-    return heatmap_dict
+    heatmap_data = {
+        'complete': heatmap_dict,
+        'sampled': heatmap_dict_sampled
+    }
+
+    return heatmap_data
 
 
 
 
-def plot_heatmaps(heatmap_dict, out_dir):
+def plot_heatmaps(heatmap_dicts, out_dir):
     """
     Plot VAF Heatmap, one per sample.
 
     Args:
-        heatmap_dict (str): dictionary of samples' dataframes to plot the VAF Heatmap
-        out_file (str, optional): Path to output file of the project.
+        heatmap_dicts (str): dictionary of dictionaries of complete and sampled heatmap of samples' dataframes to plot the VAF Heatmap
+        out_file (str): Path to output file of the study.
         
     """
-    for sample_id, df in heatmap_dict.items():
-        n_rows, n_cols = df.shape
-        fig, ax = plt.subplots(figsize=(max(12, n_cols*4.5), max(5, n_rows*0.2)))
-        sns.set(font_scale=0.9)
+    for data_type, heatmap_dict in heatmap_dicts.items():
+        # Create directory if it does not exist
+        type_dir = os.path.join(out_dir, data_type)
+        os.makedirs(type_dir, exist_ok=True)
         
-        # Set color palette
-        cmap = sns.light_palette("#009c8c", as_cmap=True)
-
+        print(f"Plotting {data_type} heatmaps...")
         
-        # Plot heatmap
-        heatmap = sns.heatmap(
-            df,
-            annot=False,
-            fmt=".3f",
-            cmap=cmap,
-            linewidths=0.5,
-            vmax=0.6
-        )
+        for sample_id, df in heatmap_dict.items():
+            n_rows, n_cols = df.shape
+            fig, ax = plt.subplots(figsize=(max(12, n_cols*4.5), max(5, n_rows*0.2)))
+            sns.set(font_scale=0.9)
+            
+            # Set color palette
+            cmap = sns.light_palette("#009c8c", as_cmap=True)
+            
+            # Plot heatmap
+            heatmap = sns.heatmap(
+                df,
+                annot=False,
+                fmt=".3f",
+                cmap=cmap,
+                linewidths=0.5,
+                vmax=0.6
+            )
+            
+            heatmap.xaxis.tick_top()
+            heatmap.tick_params(axis='x', which='both', pad=10, top=True, bottom=False, length=0)
+            heatmap.tick_params(axis='y', which='both', left=False, right=False, length=0)
+            heatmap.xaxis.set_label_position('top') 
+            
+            # Adjust color bar params
+            cbar = heatmap.collections[0].colorbar
+            cbar.ax.tick_params(axis='x', labelrotation=0) 
+            cbar.ax.set_xlabel("VAF", fontsize=16, labelpad=10)
+            cbar.ax.xaxis.set_label_position('top')
         
-        heatmap.xaxis.tick_top()
-        heatmap.tick_params(axis='x', which='both', pad=10, top=True, bottom=False, length=0)
-        heatmap.tick_params(axis='y', which='both', left=False, right=False, length=0)
-        heatmap.xaxis.set_label_position('top') 
-        
-        
-        # Adjust color bar params
-        cbar = heatmap.collections[0].colorbar
-        cbar.ax.tick_params(axis='x', labelrotation=0) 
-        cbar.ax.set_xlabel("VAF", fontsize=16, labelpad=10)
-        
-        # Adjust the position of the color bar label
-        cbar.ax.xaxis.set_label_position('top')
-    
-        # Set axis labels and title
-        plt.title(sample_id, fontsize=20, pad=25)
-        plt.xlabel("Clones", fontsize=16, labelpad=15)
-        plt.ylabel("Mutations", fontsize=16, labelpad=15)
-
-        os.makedirs(out_dir, exist_ok=True)
-        plt.savefig(f"{out_dir}/{sample_id}_vaf_heatmap.png", bbox_inches='tight')
-        plt.close()
+            # Set axis labels and title
+            title = f"{sample_id} ({data_type.capitalize()})"
+            plt.title(title, fontsize=20, pad=25)
+            plt.xlabel("Clones", fontsize=16, labelpad=15)
+            plt.ylabel("Mutations", fontsize=16, labelpad=15)
+            
+            plt.savefig(f"{type_dir}/{sample_id}_{data_type}_vaf_heatmap.png", bbox_inches='tight')
+            plt.close()
 
 
 
@@ -167,10 +254,12 @@ if __name__ == '__main__':
     input_parser.add_argument("--tree_df", action='store', required=True)
     input_parser.add_argument("--pvi_out", action='store', required=True)
     input_parser.add_argument("--mut_dir", action='store', required=True)
+    input_parser.add_argument("--gene_alterations", action='store', required=True)
     input_parser.add_argument("--out_dir", action='store', required=True)
+
 
     args = input_parser.parse_args()
 
 
-    heatmap_dict = build_heatmap_df(args.tree_df,args.pvi_out, args.mut_dir)
-    plot_heatmaps(heatmap_dict,args.out_dir)
+    heatmap_data = build_heatmap_df(args.tree_df, args.pvi_out, args.mut_dir, args.gene_alterations)
+    plot_heatmaps(heatmap_data, args.out_dir)
