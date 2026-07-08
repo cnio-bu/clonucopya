ac64b05f20676880e1577ca3d40feb3db962319b
Guillermo Sánchez-Cid <guisanbue@gmail.com>
2026-07-08 10:25:11 +0200

    fix color asignation

--- workflow/scripts/draw_clonal_tree.py
+++ workflow/scripts/draw_clonal_tree.py
@@ -11,30 +11,19 @@ def make_branches_thicker(node, new_size):
 
 
 def draw_subclonal_tree(nwk_file, palette, out_file):
-    """
-    Plot subclonal tree from Phyclone output.
-
-    Args:
-        nwk_file (str): Path to Phyclone's output nwk file 
-        palette (str): Path to the color palette file. One color per line in hex format
-        out_file (str): Path to output png file.
-
-    """
-
     with open(palette, 'r') as palette:
         colors = palette.readlines()
         color_palette = [color.strip() for color in colors]
 
-    t =  Tree(nwk_file, format=1) 
+    t = Tree(nwk_file, format=1)
 
-    # Define basic configuration of the tree
     ts = TreeStyle()
     ts.show_leaf_name = False
     ts.show_scale = False
 
     # Set root style
     root_style = NodeStyle()
-    root_style["size"] = 15  
+    root_style["size"] = 15
     root_style["fgcolor"] = color_palette[0]
     root_style["hz_line_type"] = 0
     root_style["hz_line_color"] = "#000"
@@ -46,40 +35,49 @@ def draw_subclonal_tree(nwk_file, palette, out_file):
     if t.name:
         root_label = faces.TextFace(t.name, fsize=12, fgcolor="black")
         t.add_face(root_label, column=0, position="branch-right")
-
+        
     # Set the nodes of the clones
     descendant_palette = color_palette[1:]
-    num_colors = len(descendant_palette)
-    color_index = 0
-    
+
+    # Clonal nodes, excluding non-tumor cell
+    clone_nodes = [node for node in t.traverse() if node != t and node.name]
+
+    # Sort clones
+    clone_nodes_sorted = sorted(clone_nodes, key=lambda n: int(n.name))
+
+    # Map clone -> color
+    clone_color_map = {
+        node.name: descendant_palette[i % len(descendant_palette)]
+        for i, node in enumerate(clone_nodes_sorted)
+    }
+
     for node in t.traverse():
-        # Pass root node because it has its own style
         if node == t:
             continue
-            
+
         custom_style = NodeStyle()
         custom_style["size"] = 15
         custom_style["hz_line_type"] = 0
         custom_style["hz_line_color"] = "#000"
-        
+
         # Asign a color to each color from the clonucopya's palette
-        custom_style["fgcolor"] = descendant_palette[color_index % len(color_palette)]
-        color_index += 1      
+        custom_style["fgcolor"] = clone_color_map[node.name]
         node.set_style(custom_style)
-        
+
         # Add labels to clone nodes
         if node.name:
             label = faces.TextFace(f"c{node.name}", fsize=12, fgcolor="black")
             node.add_face(label, column=0, position="branch-right")
-    
-        # Change branch thickness
-        make_branches_thicker(t, 1)
-        
+            
+    # Change branch thickness
+    make_branches_thicker(t, 1)
+
     # Set environment variable for offscreen rendering
     os.environ["QT_QPA_PLATFORM"] = "offscreen"
-    
+
     # Save the tree as a PNG file
     t.render(out_file, w=800, units="px", tree_style=ts)
+
 
 if __name__ == '__main__':
     input_parser = argparse.ArgumentParser()
